c91b97e39acf7b27cf13ff74ee10ff68faf1daaf
kunthaicorarl <kunthai.corarl@gmail.com>
2017-11-20 00:45:10 +0700

    Update ProductDirective.js

--- directives/ProductDirective.js
+++ directives/ProductDirective.js
@@ -1,1 +1,10 @@
 
+angular.module("myApp").directive("productDirective",[function(){
+return{
+  restrict:"A",
+  scope:{},
+  templateUrl:"../directives/ProductDirective.html",
+  link:function(scope,element,attr){ 
+  }
+};
+}]);
