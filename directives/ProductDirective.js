8049bf2be476db7af79edcc0fab328d7fe071990
kunthaicorarl <kunthai.corarl@gmail.com>
2017-11-20 00:56:34 +0700

    Update ProductDirective.js

--- directives/ProductDirective.js
+++ directives/ProductDirective.js
@@ -2,8 +2,7 @@
 angular.module("myApp").directive("productDirective",["$compile","$templateRequest",function($compile,$templateRequest){
 return{
   restrict:"A",
-  scope:{},
-  templateUrl:"https://1cloudmini.github.io/angularjspostdemo/directives/ProductDirective.html",
+  scope:{}
   link:function(scope,element,attr){ 
      var newTemplate=$templateRequest("https://1cloudmini.github.io/angularjspostdemo/directives/ProductDirective.html");
       var content = $compile(newTemplate)(scope);
