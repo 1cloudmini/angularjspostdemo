2b78ab428b5ad9b23d79d29be7a4c229ec3e9d18
Romdul Tech <romdul.tech@gmail.com>
2017-11-29 10:00:27 +0700

    update

--- ProductService.js
+++ ProductService.js
@@ -10,7 +10,7 @@ angular.module("myApp").factory("ProductService",["$http","API_URI",function($ht
                      return $http({
                              url: apiUrl, 
                              method: "GET",
-                             data: para
+                             data:paramter
                             });
                 }
           };
